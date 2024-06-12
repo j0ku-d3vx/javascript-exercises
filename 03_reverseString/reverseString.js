8a8e7e9a1b8291783f434a66aca49b89bcbe5666
j0ku-d3vx <161225665+j0ku-d3vx@users.noreply.github.com>
2024-06-12 20:52:20 +0200

    js-exercise03 beendet

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(characters) {
+    newString = "";
 
+    for(let count = characters.length-1; count >= 0; count--){
+        newString += characters[count];
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
