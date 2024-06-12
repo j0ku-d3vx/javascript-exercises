937c5f3911e785c1fffd9bdd8de9672f298d44da
j0ku-d3vx <161225665+j0ku-d3vx@users.noreply.github.com>
2024-06-12 20:40:27 +0200

    js-exercise02 beendet

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(odin, num) {
+    let string = "";
 
+    if(num >= 0){
+        for(let i = 0; i<num; i++){
+            string += odin;
+        }
+        return string;
+    } else {
+        return "ERROR";
+    }
+    
 };
 
 // Do not edit below this line
