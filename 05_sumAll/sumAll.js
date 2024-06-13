ae8ea8575639328046cda888695cd10ec5803f1a
j0ku-d3vx <161225665+j0ku-d3vx@users.noreply.github.com>
2024-06-13 20:51:08 +0200

    js-exercise05 finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(zahl1, zahl2) {
+    if(!Number.isInteger(zahl1) || !Number.isInteger(zahl2)) return "ERROR";
+    if(zahl1 < 0 || zahl2 < 0) return "ERROR";
+    if(zahl2 < zahl1){
+        let help = zahl1;
+        zahl1 = zahl2;
+        zahl2 = help;
+    }
 
+    let sum = 0;
+    for(let i = zahl1; i <= zahl2; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
