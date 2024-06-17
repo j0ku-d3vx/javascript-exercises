526cca36f6fc3337c4d9307dd8fac3e9c290783d
j0ku-d3vx <161225665+j0ku-d3vx@users.noreply.github.com>
2024-06-17 15:31:00 +0200

    javascript-exercise-11 finished

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    let titles = [];
+      
+      for(let key of arr){
+        titles.push(key.title);
+      }
 
+      return titles;
 };
 
 // Do not edit below this line
