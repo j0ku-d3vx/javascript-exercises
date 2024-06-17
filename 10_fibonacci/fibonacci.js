4fa7a52abb24b41a44db35ef40833f0b1f0c5166
j0ku-d3vx <161225665+j0ku-d3vx@users.noreply.github.com>
2024-06-17 15:12:48 +0200

    javascript-exercise-10 finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const fib = [0,1];
 
+    if(num >= 0 && num !== isNaN(num)){
+        for(let i = 2; i <= num; i++){
+            fib[i] = fib[i - 2] + fib[i - 1];
+        }
+    
+        return fib[num];
+    } else {
+        return "OOPS";
+    }
+    
 };
 
 // Do not edit below this line
