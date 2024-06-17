61c8a26801a686c1e5bf138fb50ec6f724f1b892
j0ku-d3vx <161225665+j0ku-d3vx@users.noreply.github.com>
2024-06-17 14:14:33 +0200

    javascript-exercise 08 finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  const mapped = arr.map(item => {
+    sum += item;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let multi = 1;
+  const mapped = arr.map(item => {
+    multi *= item;
+  });
+  return multi;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num === 0 || num === 1){
+    return 1;
+  }
+  for(let i = num - 1; i >= 1; i--){
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
